76354876521fdb7e76751a7935a0fef2c737b995
Brian Newill <103142588+SojuVR@users.noreply.github.com>
2022-11-29 03:43:29 +0000

    added more code

--- dateType.h
+++ dateType.h
@@ -1,5 +1,8 @@
 #ifndef DATETYPE_H
 #define DATETYPE_H
+#include <string>
+
+using namespace std;
 
 class dateType
 {
@@ -7,18 +10,48 @@ class dateType
   void setMonth(int);
   void setDay(int);
   void setYear(int);
-  void addDay(int);
-  int daysPassed();
-  int daysLeft();
-  int lastDayOfMonth(int);
-  bool leapYear(int y);
+  void addDay(int); //adds given amount of days
+  int daysPassed(); //days since day 1 of year
+  int daysLeft(); //find how many days left in year
+  int lastDayOfMonth(int); //finds how many days is in current month
+  bool leapYear(); //finds if it is a leap year
   int getMonth() {return month;}
   int getDay() {return day;}
   int getYear() {return year;}
   void print();
 
-  dateType();
-  dateType(int, int, int); 
+  dateType(); //constructor
+  dateType(int, int, int); //constructor with parameters
+
+  class invalidYear
+  {
+    public:
+    invalidYear() {message = "Year must be 1900 or greater.";}
+    string what() {return message;}
+
+    private:
+    string message;
+  };
+
+  class invalidMonth
+  {
+    public:
+    invalidMonth() {message = "Month must be between 1 and 12.";}
+    string what() {return message;}
+
+    private:
+    string message;
+  };
+
+  class invalidDay
+  {
+    public:
+    invalidDay() {message = "Day must be between 1 and max day of month.";}
+    string what() {return message;}
+
+    private:
+    string message;
+  };
 
   private:
   int month;
